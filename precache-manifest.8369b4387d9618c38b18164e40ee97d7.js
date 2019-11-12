97f99425bda03515018944de751744e693774f6e
Reshma Rubugunday <reshmakw@gmail.com>
2019-11-12 12:45:33 -0800

    Updates

---
+++ precache-manifest.8369b4387d9618c38b18164e40ee97d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283fef21698832ba1b63f01c8acadeb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "837c741f1a0766d69ec8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ff7b00600d233d974e30",
+    "url": "/static/js/2.d20f16af.chunk.js"
+  },
+  {
+    "revision": "837c741f1a0766d69ec8",
+    "url": "/static/js/main.8312c16e.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
